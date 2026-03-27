02062101004fef3f5772bcc523e28f7abf10c548
KZTP47 <karokh550@hotmail.com>
2026-03-27 16:07:34 +0100

    Fix arithmetic methods in Calculator class
    Refactor arithmetic methods to use parameters instead of hardcoded values.

--- src/main/java/systementor/cidemo1/Calculator.java
+++ src/main/java/systementor/cidemo1/Calculator.java
@@ -3,21 +3,21 @@ package systementor.cidemo1;
 public class Calculator {//denna var bortkommenterade, vilket gjorde att vi fick: class, interface, enum, or record expected, vilket är Java's way of saying att den fann code (mina methods) som inte var i en class.
 
     public int add(int a, int b) {
-        return a + b + 4;
+        return a + b;//här hade man poisonat logiken genom att lägga till ett hårdkodat värde i slutet
     }
 
     public int subtract(int a, int b) {
-        return 10;
+        return a - b;//här hade man hårdkodat ett värde
     }
 
     public int multiply(int a, int b) {
-        return a * b * 100;
+        return a * b;
     }
 
     public int divide (int a, int b) {
         if (b == 0){
             throw new IllegalArgumentException("Divide by zero");
         }
-        return a / 3;
+        return a / b;//här hade man hårdkodat en 3a
     }
 }
